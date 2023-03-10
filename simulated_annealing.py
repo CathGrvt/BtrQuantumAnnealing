c3ff04315f8c95f0e4d0871e017b8b48325eb09a
CathGrvt <grivot.cath@gmail.com>
2023-03-10 13:41:36 +0100

    negative value for sampleset.first.energy

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,24 +1,54 @@
-import dimod
-import numpy as np
 import toymodel_3d as toy
 import dp_hamiltonian as ham
+import numpy as np
 import matplotlib.pyplot as plt
+import dimod
+from dwave.samplers import SimulatedAnnealingSampler
 
+sampler = SimulatedAnnealingSampler()
 
-# Set up toy event and generate Hamiltonian
 N_MODULES = 3
-N_TRACKS = 2
+N_TRACKS = 3
+
 LX = 2
 LY = 2
 SPACING = 1
+
 detector = toy.generate_simple_detector(N_MODULES, LX, LY, SPACING)
 event = toy.generate_event(detector, N_TRACKS, theta_max=np.pi / 50, seed=1)
+
+fig = plt.figure()
+fig.set_size_inches(12, 6)
+ax = plt.axes(projection='3d')
+event.display(ax)
+ax.view_init(vertical_axis='y')
+fig.set_tight_layout(True)
+ax.axis('off')
+ax.set_title(f"Generated event\n{len(event.modules)} modules\n{len(event.tracks)} tracks - {len(event.hits)} hits")
+plt.show()
+
 params = {
     'alpha': 0.0,
     'beta': 1.0,
     'lambda': 100.0,
 }
 A, b, components, segments = ham.generate_hamiltonian(event, params)
+
+fig = plt.figure()
+fig.set_size_inches(12, 6)
+ax = plt.axes(projection='3d')
+event.display(ax, show_tracks=False)
+
+for segment in segments:
+    segment.display(ax)
+
+ax.view_init(vertical_axis='y')
+fig.set_tight_layout(True)
+ax.axis('off')
+
+ax.set_title(f"{len(segments)} segments generated")
+
+plt.show()
 
 # Define the BQM and sampler for simulated annealing
 bqm = dimod.BinaryQuadraticModel.from_qubo(A)
@@ -28,21 +58,6 @@ sampler = dimod.SimulatedAnnealingSampler()
 response = sampler.sample(bqm, num_reads=1000)
 best_sample = response.record.sample[0]
 
-# Visualize the reconstructed tracks
-fig = plt.figure()
-fig.set_size_inches(12, 6)
-ax = plt.axes(projection='3d')
-event.display(ax, show_tracks=False)
-
-reconstructed_tracks = []
-for i in range(N_TRACKS):
-    track = [j for j in range(len(components)) if best_sample[j + i*len(components)] == 1]
-    reconstructed_tracks.append(track)
-    for component in [components[j] for j in track]:
-        component.display(ax, color='g')
-
-ax.view_init(vertical_axis='y')
-fig.set_tight_layout(True)
-ax.axis('off')
-ax.set_title(f"{N_TRACKS} tracks reconstructed")
-plt.show()
+sampleset = sampler.sample(bqm)
+sampleset = sampler.sample(bqm, beta_range=[.1, 4.2], beta_schedule_type='linear')
+print(sampleset.first.energy)
